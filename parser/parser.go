f5fd3d4618a61148f9f9cd5a5e2b5c8bc1396206
Marcelo Eduardo <marceloer2011@gmail.com>
2024-04-21 21:07:30 -0300

    refactor: readjusting test function order

--- parser/parser.go
+++ parser/parser.go
@@ -146,7 +146,7 @@ func (p *Parser) parseReturnStatement() ast.Statement {
 }
 
 func (p *Parser) parseExpressionStatement() ast.Statement {
-    defer untrace(trace("parseExpressionStatement"))
+    // defer untrace(trace("parseExpressionStatement"))
 
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 
@@ -166,7 +166,7 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 }
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
-    defer untrace(trace("parseExpression"))
+    // defer untrace(trace("parseExpression"))
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -191,7 +191,7 @@ func (p *Parser) parseIdentifier() ast.Expression {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
-    defer untrace(trace("parseIntegerLiteral"))
+    // defer untrace(trace("parseIntegerLiteral"))
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
@@ -205,7 +205,7 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
-    defer untrace(trace("parsePrefixExpression"))
+    // defer untrace(trace("parsePrefixExpression"))
 	expression := &ast.PrefixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
@@ -234,7 +234,7 @@ func (p *Parser) curPrecedence() int {
     return LOWEST
 }
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
-    defer untrace(trace("parseInfixExpression"))
+    // defer untrace(trace("parseInfixExpression"))
     expression := &ast.InfixExpression{
         Token: p.curToken,
         Operator: p.curToken.Literal,
