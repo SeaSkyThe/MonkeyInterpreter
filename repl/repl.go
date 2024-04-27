cf1ca6c97967803d464833903b3ddff452e344c1
Marcelo Eduardo <marceloer2011@gmail.com>
2024-04-27 14:53:43 -0300

    feat: upgrading REPL

--- repl/repl.go
+++ repl/repl.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 
 	"github.com/SeaSkyThe/MonkeyInterpreter/lexer"
-	"github.com/SeaSkyThe/MonkeyInterpreter/token"
+	"github.com/SeaSkyThe/MonkeyInterpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -27,11 +27,40 @@ func Start(in io.Reader, out io.Writer) {
 			fmt.Println("\033[H\033[2J")
 		} else {
 			l := lexer.New(line)
+			p := parser.New(l)
 
-			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-				fmt.Printf("%+v\n", tok)
+			program := p.ParseProgram()
+
+			if len(p.Errors()) != 0 {
+				printParserErrors(out, p.Errors())
+				continue
 			}
 
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+
 		}
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
